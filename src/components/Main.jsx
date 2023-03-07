5812cc9c949875c765eaefe1c5093a7c1f88e845
Alexandra <alexmiriamd@gmail.com>
2023-03-07 13:34:30 -0700

    working on homepage flex

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,19 @@ import { Navbar } from ".";
 
 const Main = () => {
     return(
-        <div id="main">
+        <div class="main">
             <Navbar />
+            <div id="mainText">
+                <h1 class = "name">
+                    Alexandra Danielson !!!
+                </h1>
+                <h2>
+                    Web Developer and Artist
+                </h2>
+                <h3>
+                    I design and code beautiful things
+                </h3>
+            </div>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 const Navbar = () => {
-  return (  //nothing linked yet. 
+  return (                //nothing linked yet. 
     <div id="navbar">
-      <h1>
-      Alexandra Danielson
-      </h1>
       <div id="navigation">   
-        <h1> home </h1>
+        {/* <h1> home </h1> */}
         <h1> about </h1>
         <h1> portfolio </h1>
       </div>
