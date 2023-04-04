648e24a745d6a0e97e99a6d9855912082f05ca8a
Alexandra <alexmiriamd@gmail.com>
2023-04-03 21:09:47 -0600

    typing animation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,14 +5,17 @@ const Main = () => {
     return(
         <div class="main">
             <Navbar />
-            <div id="mainText">
-                <h1 class = "name">
-                    Alexandra Danielson !!!
+            <div class ="main_text">
+                <h1 id = "name">
+                    Alexandra M. Danielson
                 </h1>
                 <h2>
-                    Web Developer and Artist
+                    Web Developer
                 </h2>
-                <h3>
+                <h2> 
+                    Artist
+                </h2>
+                <h3 id="design_code">
                     I design and code beautiful things
                 </h3>
             </div>
